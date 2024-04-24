e3663607effc638ece06d3683bf49127ce8887b2
Marcos von Stremayr <vonstremayrmarcos@gmail.com>
2024-04-23 21:10:05 -0300

    Update site URL and add assets directory to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https:/marcosvonstremayr.tech",
   integrations: [mdx(), sitemap(), tailwind()],
+  assets: "./public",
 });
