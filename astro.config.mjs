1fd55a33d94557e68f32bd2392f21b32df579821
Marcos von Stremayr <vonstremayrmarcos@gmail.com>
2024-04-23 22:02:08 -0300

    Added missing import. Fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,19 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: "https:/marcosvonstremayr.tech",
-  integrations: [mdx(), 
+  integrations: [
+    mdx(), 
     sitemap(), 
     tailwind(), 
     partytown({
     config: {
       forward: ["dataLayer.push"],
-      },
-    }),],
+        },
+    }),
+  ],
   assets: "./public",
 });
